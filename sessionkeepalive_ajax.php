a99965625c755f7b5982a8ea79da09b02f2f0f79
Alexander Bias <bias@alexanderbias.de>
2021-09-29 21:15:01 +0200

    Remove the deprecated print_error() function

--- sessionkeepalive_ajax.php
+++ sessionkeepalive_ajax.php
@@ -32,10 +32,7 @@ require(__DIR__ . '/../../config.php');
 
 // Require the session key - want to make sure that this isn't called
 // maliciously to keep a session alive longer than intended.
-if (!confirm_sesskey()) {
-    header('HTTP/1.1 403 Forbidden');
-    print_error('invalidsesskey');
-}
+require_sesskey();
 
 // Update the session.
 \core\session\manager::touch_session(session_id());
