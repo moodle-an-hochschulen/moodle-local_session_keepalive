0fbfb136914727e8b3bfef7ba321a5b1f4a39467
Alexander Bias <bias@alexanderbias.de>
2025-10-19 09:48:57 +0200

    Make codechecker happy again

--- settings.php
+++ settings.php
@@ -28,20 +28,26 @@ global $CFG;
 
 if ($hassiteconfig) {
     // Create new settings page.
-    $page = new admin_settingpage('local_session_keepalive',
-            get_string('pluginname', 'local_session_keepalive', null, true));
+    $page = new admin_settingpage(
+        'local_session_keepalive',
+        get_string('pluginname', 'local_session_keepalive', null, true)
+    );
 
     if ($ADMIN->fulltree) {
         // Add general functionality heading.
-        $page->add(new admin_setting_heading('local_session_keepalive/generalfunctionalityheading',
-                get_string('setting_generalfunctionalityheading', 'local_session_keepalive', null, true),
-                ''));
+        $page->add(new admin_setting_heading(
+            'local_session_keepalive/generalfunctionalityheading',
+            get_string('setting_generalfunctionalityheading', 'local_session_keepalive', null, true),
+            ''
+        ));
 
         // Create enable control widget.
-        $page->add(new admin_setting_configcheckbox('local_session_keepalive/enable',
-                get_string('setting_enable', 'local_session_keepalive', null, true),
-                get_string('setting_enable_desc', 'local_session_keepalive', null, true),
-                0));
+        $page->add(new admin_setting_configcheckbox(
+            'local_session_keepalive/enable',
+            get_string('setting_enable', 'local_session_keepalive', null, true),
+            get_string('setting_enable_desc', 'local_session_keepalive', null, true),
+            0
+        ));
 
         // Create keepalive interval control widget.
         $choices = [1 => 1, 2 => 2, 5 => 5, 10 => 10, 30 => 30, 60 => 60, 90 => 90,
@@ -50,25 +56,33 @@ if ($hassiteconfig) {
         $link = \core\output\html_writer::link($sessionhandlingurl, get_string('sessionhandling', 'core_admin'));
         $sessiontimeoutmin = floor(($CFG->sessiontimeout / 60));
         $sessiontimeoutwarningmin = floor(($CFG->sessiontimeoutwarning / 60));
-        $page->add(new admin_setting_configselect('local_session_keepalive/keepaliveinterval',
-                get_string('setting_keepaliveinterval', 'local_session_keepalive', null, true),
-                get_string('setting_keepaliveinterval_desc',
-                        'local_session_keepalive',
-                        ['minutes' => $sessiontimeoutmin, 'page' => $link],
-                        true).
-                '<br /><br />'.
-                get_string('setting_keepalivetimeoutwarningnote',
-                        'local_session_keepalive',
-                        ['minutes' => $sessiontimeoutwarningmin, 'page' => $link],
-                        true),
-                60,
-                $choices));
+        $page->add(new admin_setting_configselect(
+            'local_session_keepalive/keepaliveinterval',
+            get_string('setting_keepaliveinterval', 'local_session_keepalive', null, true),
+            get_string(
+                'setting_keepaliveinterval_desc',
+                'local_session_keepalive',
+                ['minutes' => $sessiontimeoutmin, 'page' => $link],
+                true
+            ) .
+                '<br /><br />' .
+                get_string(
+                    'setting_keepalivetimeoutwarningnote',
+                    'local_session_keepalive',
+                    ['minutes' => $sessiontimeoutwarningmin, 'page' => $link],
+                    true
+                ),
+            60,
+            $choices
+        ));
         unset($choices);
 
         // Add advanced settings heading.
-        $page->add(new admin_setting_heading('local_session_keepalive/advancedsettingsheading',
-                get_string('setting_advancedsettingsheading', 'local_session_keepalive', null, true),
-                ''));
+        $page->add(new admin_setting_heading(
+            'local_session_keepalive/advancedsettingsheading',
+            get_string('setting_advancedsettingsheading', 'local_session_keepalive', null, true),
+            ''
+        ));
 
         // Create keepalive time control widgets.
         $days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
@@ -77,22 +91,28 @@ if ($hassiteconfig) {
                     // Don't use string lazy loading here because the string will be directly used and
                     // would produce a PHP warning otherwise.
         }
-        $page->add(new admin_setting_configmulticheckbox2('local_session_keepalive/keepaliveweekdays',
-                get_string('setting_keepaliveweekdays', 'local_session_keepalive', null, true),
-                get_string('setting_keepaliveweekdays_desc', 'local_session_keepalive', null, true),
-                $choices,
-                $choices));
+        $page->add(new admin_setting_configmulticheckbox2(
+            'local_session_keepalive/keepaliveweekdays',
+            get_string('setting_keepaliveweekdays', 'local_session_keepalive', null, true),
+            get_string('setting_keepaliveweekdays_desc', 'local_session_keepalive', null, true),
+            $choices,
+            $choices
+        ));
         unset($choices);
-        $page->add(new admin_setting_configtime('local_session_keepalive/keepalivestart',
-                'keepalivestartmin',
-                get_string('setting_keepalivestart', 'local_session_keepalive', null, true),
-                '',
-                ['h' => 0, 'm' => 0]));
-        $page->add(new admin_setting_configtime('local_session_keepalive/keepaliveend',
-                'keepaliveendmin',
-                get_string('setting_keepaliveend', 'local_session_keepalive', null, true),
-                get_string('setting_keepaliveend_desc', 'local_session_keepalive', null, true),
-                ['h' => 0, 'm' => 0]));
+        $page->add(new admin_setting_configtime(
+            'local_session_keepalive/keepalivestart',
+            'keepalivestartmin',
+            get_string('setting_keepalivestart', 'local_session_keepalive', null, true),
+            '',
+            ['h' => 0, 'm' => 0]
+        ));
+        $page->add(new admin_setting_configtime(
+            'local_session_keepalive/keepaliveend',
+            'keepaliveendmin',
+            get_string('setting_keepaliveend', 'local_session_keepalive', null, true),
+            get_string('setting_keepaliveend_desc', 'local_session_keepalive', null, true),
+            ['h' => 0, 'm' => 0]
+        ));
     }
 
     // Add settings page to navigation tree.
